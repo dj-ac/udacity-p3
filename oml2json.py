79acdef8b14077b9215f1b1765bac668b0cb4e86
ac <dj-ac@github>
2017-06-24 20:04:53 -0700

    Reorganized and changed code to process all tags

--- oml2json.py
+++ oml2json.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
 import xml.etree.cElementTree as ET
 import argparse
-import pprint
 import re
 import codecs
 import traceback
 import json
+import sys
 import warnings
 
-lower = re.compile(r'^([a-z]|_)*$')
-lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
-problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+class ScriptConst:
+    lower_re = re.compile(r'^([a-z]|_)*$')
+    lower_colon_re = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+    problemchars_re = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
 CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
 
@@ -22,20 +22,12 @@ CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
         - attributes for latitude and longitude should be added to a "pos" array,
             for use in geospacial indexing. Make sure the values inside "pos" array are floats
             and not strings. 
-- if the second level tag "k" value contains problematic characters, it should be ignored
-- if the second level tag "k" value starts with "addr:", it should be added to a dictionary "address"
-- if the second level tag "k" value does not start with "addr:", but contains ":", you can
-  process it in a way that you feel is best. For example, you might split it into a two-level
-  dictionary like with "addr:", or otherwise convert the ":" to create a valid key.
-- if there is a second ":" that separates the type/direction of a street,
-  the tag should be ignored
+
 - for "way" specifically: "node_refs": ["305896090", "1719825889"]
 """
 
 def shape_element(element):
     node = { "created" : {} }
-    
-    key_subkey = re.compile(r'^(([a-z]|_)*):(([a-z]|_)*)$')
 
     if element.tag == "node" or element.tag == "way" :
         node['type'] = element.tag
@@ -52,30 +44,7 @@ def shape_element(element):
         contentNodes = element.getchildren()
         for n in contentNodes:
             if n.tag == "tag":
-                tagKey = n.get('k')
-                tagVal = n.get('v')
-                if problemchars.match(tagKey):
-                    continue
-                
-                if tagKey.count(':') > 1:
-                    continue
-                
-                key_match = key_subkey.match(tagKey)
-                if key_match:
-                    tagKeyPre = key_match.group(1)
-                    tagKeyVal = key_match.group(3)
-
-                    if tagKeyPre == "addr":
-                        if 'address' not in node:
-                            node['address'] = {}
-                        node['address'][tagKeyVal] = tagVal
-                    else:
-                        if tagKeyPre not in node:
-                            node[tagKeyPre] = {}
-                        elif type(node[tagKeyPre]) == dict:
-                            node[tagKeyPre][tagKeyVal] = tagVal
-                        else:
-                            warnings.warn("ELEMENT: '{0}', ID: '{1}' - ignoring tag '{2}' due to conflict with existing '{3}' key".format(element.tag, element.get('id'), tagKey, tagKeyPre))
+                process_tag(element, node)
             elif n.tag == "nd" and element.tag == "way":
                 if 'node_refs' not in node:
                     node['node_refs'] = []
@@ -85,55 +54,57 @@ def shape_element(element):
     else:
         return None
 
+def process_tag(tag_element, out_json_node):
+    """Processes nested 'tag' elements of an XML node and adds them
+       to the JSON representation
+
+       - if the second level tag "k" value starts with "addr:", it should be added to a dictionary "address"
+       - all other tags are to be added to the 'tags' dictionary
+    """
+    tag_key = tag_element.get('k')
+    tag_val = tag_element.get('v')
+
+    if ScriptConst.problemchars_re.match(tag_key):
+        return
+
+    addr_marker = 'addr:'
+    if tag_key.startswith(addr_marker):
+        #handle addr:* entries
+        tag_key_val = tag_key[(len(addr_marker)):]
+
+        if 'address' not in out_json_node: out_json_node['address'] = {}
+        out_json_node['address'][tag_key_val] = tag_val
+    else:
+        if 'tags' not in out_json_node: out_json_node['tags'] = {}
+        out_json_node['tags'][tag_key] = tag_val
+
 
 def process_map(file_in, pretty = False):
-    # You do not need to change this file
+    elements_processed = 0
+    elements_failed = 0
+
     file_out = "{0}.json".format(file_in)
     data = []
     with codecs.open(file_out, "w") as fo:
         for _, element in ET.iterparse(file_in):
             try:
                 el = shape_element(element)
+                elements_processed += 1
                 if el:
                     data.append(el)
                     if pretty:
                         fo.write(json.dumps(el, indent=2)+"\n")
                     else:
                         fo.write(json.dumps(el) + "\n")
+                stdout_map_processing_status(elements_processed, elements_failed)
             except Exception as e:
+                elements_failed += 1
                 print element, element.get('id')
                 print e
                 traceback.print_exc()
 
+    stdout_map_processing_status(elements_processed, elements_failed, refresh=False)
     return data
-
-def test():
-    data = process_map('example.osm', True)
-    #pprint.pprint(data)
-    
-    correct_first_elem = {
-        "id": "261114295", 
-        "visible": "true", 
-        "type": "node", 
-        "pos": [41.9730791, -87.6866303], 
-        "created": {
-            "changeset": "11129782", 
-            "user": "bbmiller", 
-            "version": "7", 
-            "uid": "451048", 
-            "timestamp": "2012-03-28T18:31:23Z"
-        }
-    }
-    
-    pprint.pprint( data[-1] )
-    
-    assert data[0] == correct_first_elem
-    assert data[-1]["address"] == {
-                                    "street": "West Lexington St.", 
-                                    "housenumber": "1412"
-                                      }
-    assert data[-1]["node_refs"] == [ "2199822281", "2199822390",  "2199822392", "2199822369", 
-                                    "2199822370", "2199822284", "2199822281"]
 
 def parse_cmdline():
     parser = argparse.ArgumentParser(description='Converts OSM openstreet data to JSON')
@@ -142,7 +113,13 @@ def parse_cmdline():
                         metavar='<OSM-FILE>', dest='osm')
     return parser.parse_args()
 
+def stdout_map_processing_status(elements_processed, elements_failed, refresh=True):
+    line_end = "\r" if refresh else ""
+    status_msg = "Status:  [ processed: %d;  failed: %d ]%s" % (elements_processed, elements_failed, line_end)
+    sys.stdout.write(status_msg)
+    sys.stdout.flush()
+
+
 if __name__ == "__main__":
     args = parse_cmdline()
     data = process_map(args.osm)
-    #test()
