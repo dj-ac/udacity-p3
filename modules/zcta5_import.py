338890b37d6286894baa661afaf0f4aef15616c3
ac <dj-ac@github>
2017-08-14 00:04:20 -0700

    zcta data processing library

---
+++ modules/zcta5_import.py
@@ -0,0 +1,18 @@
+"""
+The module allows to load Polygon data from CSV file generated with
+https://github.com/censusreporter/census-shapefile-utils
+"""
+import re
+
+def load_polygondata(polygon_str):
+    """Loads polygon data from string POLYGON((lon lat, lon lat, ...))
+    https://stackoverflow.com/questions/38532816/how-to-transform-a-polygon-string-on-a-real-polygon"""
+    polygon_re = re.compile(r"^POLYGON \(\((.*)\)\)")
+    polygon_match = polygon_re.match(polygon_str)
+
+    if polygon_match is not None:
+        coord_str = polygon_match.groups()[0]
+
+        # parse string of coordinates into a list of float pairs
+        return [[float(s) for s in p.split()] for p in coord_str.split(";")]
+    return None
