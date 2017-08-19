df49d67844760bccc2504f673a299a1e7e3e475e
= <=>
2017-08-19 11:40:30 -0700

    Reimplemented using shapely

--- modules/zcta5_import.py
+++ modules/zcta5_import.py
@@ -2,29 +2,21 @@
 The module allows to load Polygon data from CSV file generated with
 https://github.com/censusreporter/census-shapefile-utils
 """
-import re
 import csv
 import logging
 import numpy as np
-import matplotlib.path as mplPath
+from shapely.wkt import loads
+from shapely.geometry import Point
 
-def load_polygon(polygon_str):
+def try_load_polygon(polygon_str):
     """Loads polygon data from string POLYGON((lon lat, lon lat, ...))
     https://stackoverflow.com/questions/38532816/how-to-transform-a-polygon-string-on-a-real-polygon"""
-    polygon_re = re.compile(r"^POLYGON \(\((.*)\)\)")
-    polygon_match = polygon_re.match(polygon_str)
+    return loads(polygon_str)
 
-    if polygon_match is not None:
-        coord_str = polygon_match.groups()[0]
-
-        # parse string of coordinates into a list of float pairs
-        return [[float(s) for s in p.split()] for p in coord_str.split(",")]
-    return None
-
-def coord_within_ziparea(latlong, poly_array):
+def coord_within_ziparea(latlong, shapely_zcta_poly):
     """Determines if a point is withing the polygon for a zip code area
     https://stackoverflow.com/questions/16625507/python-checking-if-point-is-inside-a-polygon"""
-    return mplPath.Path(np.array(poly_array)).contains_point(latlong)
+    return Point(latlong).within(shapely_zcta_poly)
 
 def load_zcta5_data(zcta_csv_file):
     """Loads zcta5 data into a dictionary ZIP=>POLYGON from CSV data file extracted with
@@ -37,7 +29,7 @@ def load_zcta5_data(zcta_csv_file):
         z_poly_str = row['GEOMETRY']
         if z_key not in zcta_data:
             try:
-                z_poly = load_polygon(z_poly_str)
+                z_poly = try_load_polygon(z_poly_str)
                 if z_poly is not None:
                     zcta_data[z_key] = z_poly
                 else:
